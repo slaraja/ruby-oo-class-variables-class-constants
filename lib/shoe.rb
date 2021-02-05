ebccc3cd27c5ed4aba2d4a15abc1e8849b11dd4c
Stephanie La Raja <stephanielaraja@gmail.com>
2021-02-05 07:23:51 -0500

    commit tree 287f1cce9f3bfd4db6d5f07e51cdabca7bf70e64

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,14 @@
 class Shoe
+
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    BRANDS << brand
+    BRANDS.uniq!
   end
 
   def cobble
@@ -11,4 +16,4 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
-end
+end 
